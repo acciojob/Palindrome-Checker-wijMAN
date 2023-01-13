9866feabd85ba8f7a5eacc00b4e120045beb7f3f
wijman <wish6242@gmail.com>
2023-01-13 21:26:44 +0530

    1st commit

--- solution.js
+++ solution.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+    let s=str.toLowerCase();
+    let i=0, j=s.length-1;
+    while(j>i){
+        if(s.charAt(i)!=s.charAt(j)) return false;
+        i++; j--;
+    }
+    return true;
 }
 module.exports = palindrome
